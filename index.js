95c735a686e4606a440ee3a8f806093f14161e5e
Relten98 <diezhandinoty@gmail.com>
2021-03-06 16:20:48 -0800

    main porfolio page overhauled
    Now it is EXTREMELY slick looking

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("linkedin").addEventListener("click", function() {
+    location.href = "https://www.linkedin.com/in/ty-diezhandino-257a93208/";
+  });
+
+  document.getElementsByClassName("github").addEventListener("click", function() {
+    location.href = "https://github.com/Relten98";
+  });
