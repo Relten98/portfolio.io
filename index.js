b88285e828b64b8d637225cc72015ff04f2c865c
Relten98 <diezhandinoty@gmail.com>
2021-03-07 23:22:44 -0800

    Delete index.js
    Decided against using event listeners, since the same task could just be done in html.
    
    Listeners were only throwing errors into the console, anyways.

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByClassName("linkedin").addEventListener("click", function() {
-    location.href = "https://www.linkedin.com/in/ty-diezhandino-257a93208/";
-  });
-
-  document.getElementsByClassName("github").addEventListener("click", function() {
-    location.href = "https://github.com/Relten98";
-  });
